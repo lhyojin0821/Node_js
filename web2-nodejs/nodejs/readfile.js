d42e628a9f6dca11f5a59e8c1f975f57c4531007
임효진 <imhyojin@imhyojin-ui-MacBookAir.local>
2021-05-28 17:04:14 +0900

    output

--- web2-nodejs/nodejs/readfile.js
+++ web2-nodejs/nodejs/readfile.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
 
-fs.readFile('sample.txt','utf8',function(err, data){
-console.log(data);
+fs.readFile('sample.txt', 'utf8', function(err, data){
+    console.log(data);
 });
